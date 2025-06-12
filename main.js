24840f91321db072881e0d2fc6c8c134a9bfc94b
Pblshman <duiberhernando@hotmail.com>
2025-06-12 04:57:47 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let menu = document.getElementById('menu');
+let navBar = document.getElementById('navbar');
+
+if(menu){
+
+    menu.addEventListener('click', () => {
+
+        navBar.classList.add('activo');
+
+    })
+
+    console.log("few");
+
+}
